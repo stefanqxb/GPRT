0c4d760743edfcb763a546b3a4f9017821c167f1
Heydar <heydar@gmail.com>
2015-08-24 20:57:48 +0200

    Minor Updates

--- scr/Package/script.py
+++ scr/Package/script.py
@@ -8,14 +8,18 @@ import data_tools
 import feature_extraction
 
 if __name__=="__main__":
-	peptides = data_tools.read_data() # elude or bow
-	#fe = feature_extraction.feature_extractor(peptides)
-	#voc = fe.bow_voc(20)
-
-#jully = feature_extraction.peptide.bow_descriptor(peptides[0], voc)
+	peptides = data_tools.read_data() 
 
 
+	# The peptides that are supposed to be used for building feature extraction model are given to
+	# feature extractor. Later these peptides will be only the training set.
+	fe = feature_extraction.feature_extractor(peptides)
 
+	# Elude model is built only based on the peptides given to feature_extractor
+	em = fe.get_elude_model()
+	# Bow Vocabulary is built only based on the peptides that are given to feature_extractor
+	voc = fe.get_bow_voc( 25 );
 
-	#fe.gen_features_parallel( voc )
-
+	# These descritors are not normalized. A seperate module will be responsible for the normalization.
+	elude_desc = peptides[0].elude_descriptor( em )
+	bow_desc = peptides[0].bow_descriptor( voc )
