7553fb3cf81c8e13711f00ba256164ce04f79633
stefanqxb <stefanqxb@gmail.com>
2015-08-31 14:07:32 +0200

    added

--- scr/Package/ml_tools.py
+++ scr/Package/ml_tools.py
@@ -4,23 +4,28 @@ import numpy as np
 import random
 import feature_extraction
 import GPy
+import math
 import matplotlib.pyplot as plt
 
 from scipy.stats.stats import pearsonr
 from sklearn import svm, grid_search
+from sklearn.cluster import KMeans as KM
 
-from joblib import Parallel, delayed  
+from joblib import Parallel, delayed
 import multiprocessing
 
-def pcv_train(i,bench):
-	model = bench.train_model(i)
-	score = bench.eval_model(i,model)
-	return score
 
-def parallel_cross_validataion( bench ):
-	num_cores = multiprocessing.cpu_count()
-	results = Parallel(n_jobs=num_cores)( delayed(pcv_train)(i,bench) for i in range(bench.parts.nfolds) )
-	return np.mean( results ), np.std( results )
+def pcv_train(i, bench):
+    model = bench.train_model(i)
+    score = bench.eval_model(i, model)
+    return score
+
+
+def parallel_cross_validataion(bench):
+    num_cores = multiprocessing.cpu_count()
+    results = Parallel(n_jobs=num_cores)(delayed(pcv_train)(i, bench) for i in range(bench.parts.nfolds))
+    return np.mean(results), np.std(results)
+
 
 class partitions:
     def __init__(self, ndata, nfolds):
@@ -71,6 +76,7 @@ class partitions:
     def get_test_part(self, ind):
         return self.test_parts[ind]
 
+
 class eval_tools:
     def mean_square_error(self, actual, predicted):
         return np.sum(np.power(actual - predicted, 2)) / len(actual)
@@ -91,17 +97,18 @@ class eval_tools:
         return time_interval
 
     def delta_t(self, actual, predicted):
-		step = 10
-		diff = abs(actual - predicted)	
-		histo = np.histogram(diff,step)
-		#histo = plt.hist(diff, step)
-		max_total = max(actual) - min(actual)
-		mtw = self.mini_time_window(histo[0], diff, step, max_total)
-		corrcoef = pearsonr(actual, predicted)
-		return mtw
+        step = 10
+        diff = abs(actual - predicted)
+        histo = np.histogram(diff, step)
+        # histo = plt.hist(diff, step)
+        max_total = max(actual) - min(actual)
+        mtw = self.mini_time_window(histo[0], diff, step, max_total)
+        corrcoef = pearsonr(actual, predicted)
+        return mtw
+
 
 class rt_model:
-    def __init__(self, feature,model_type, model, norm, voc, em):
+    def __init__(self, feature, model_type, model, norm, voc, em):
         self.feature = feature
         self.model_type = model_type
         self.model = model
@@ -126,17 +133,17 @@ class rt_model:
 
 
 class rt_benchmark:
-    def __init__(self, peptides, feature, model_type, ntrain=-1, nfolds = 5 ):
-		self.peptides = peptides
-		self.feature = feature
-		self.model_type = model_type
-		self.ntrain = ntrain
-		self.parts = partitions(len(peptides), nfolds)
-		self.parts.gen_rand_splits(0.70)
-		#self.parts.gen_cross_val();
-		
-		if ntrain < 0 or ntrain > self.parts.n_train():
-			ntrain = self.parts.n_train()
+    def __init__(self, peptides, feature, model_type, ntrain=-1, nfolds=5):
+        self.peptides = peptides
+        self.feature = feature
+        self.model_type = model_type
+        self.ntrain = ntrain
+        self.parts = partitions(len(peptides), nfolds)
+        self.parts.gen_rand_splits(0.70)
+        # self.parts.gen_cross_val();
+
+        if ntrain < 0 or ntrain > self.parts.n_train():
+            ntrain = self.parts.n_train()
 
     def train_model(self, ind):
         train_peptides = self.peptides[self.parts.get_train_part(ind)]
@@ -159,26 +166,48 @@ class rt_benchmark:
             Y = np.transpose(Y)
 
         norm = feature_extraction.normalizer()
+
         if self.feature == "elude":
             norm.normalize_maxmin(X);
         if self.model_type == "gp":
-            m = GPy.models.GPRegression(X,Y)
-            m.optimize_restarts(num_restarts=10,verbose=False)
+            m = GPy.models.GPRegression(X, Y)
+            m.optimize_restarts(num_restarts=10, verbose=False)
         elif self.model_type == "svr":
-            m = svm.SVR(C=600, gamma=0.1, coef0=0.0, degree=3, epsilon=0.1, kernel='rbf', max_iter=-1, shrinking=True, tol=0.001, verbose=False)
-            m = grid_search.GridSearchCV(m, param_grid={"C": np.linspace(100, 1000, num=10), "gamma": np.linspace(0.01,10, num = 100)})
+            m = svm.SVR(C=600, gamma=0.1, coef0=0.0, degree=3, epsilon=0.1, kernel='rbf', max_iter=-1, shrinking=True,
+                        tol=0.001, verbose=False)
+            m = grid_search.GridSearchCV(m, param_grid={"C": np.linspace(100, 1000, num=10),
+                                                        "gamma": np.linspace(0.01, 10, num=100)})
             m.fit(X, Y)
-        return rt_model(self.feature,self.model_type, m, norm, voc, em)
+        return rt_model(self.feature, self.model_type, m, norm, voc, em)
+
 
     def eval_model(self, ind, model):
         test_peptides = self.peptides[self.parts.get_test_part(ind)]
         actual = []
         predicted = []
+        std = []
         for p in test_peptides:
             actual.append(p.rt)
             v, s = model.eval(p)
             predicted.append(v)
+            std.append(s)
         actual = np.array(actual)
         predicted = np.array(predicted)
+        std = np.array(std)
         et = eval_tools()
+        estimator = KM(n_clusters = 10)
+        estimator.fit(std)
+        labels = estimator.labels_
         return et.delta_t(actual, predicted)
+
+
+
+
+
+
+
+
+
+
+
+
--- scr/Package/script.py
+++ scr/Package/script.py
@@ -2,17 +2,22 @@
 
 import numpy as np
 from matplotlib import pyplot as pp
+
 pp.ion()
 
 import data_tools
 import ml_tools
 
-if __name__== "__main__":
-	peptides = data_tools.read_data()
-	#duplicated_message = data_tools.checked_duplicated(peptides)
-	#print duplicated_message
 
-	for n in [ 100, 200, 300, 500, 1000, 2000, 3000 ]:
-		benchmark = ml_tools.rt_benchmark(peptides, 'elude','gp', n,10 )
-		m,s = ml_tools.parallel_cross_validataion(benchmark)
-		print "%d %g %g" % (n,m,s)
+
+if __name__ == "__main__":
+    peptides = data_tools.read_data()
+    # duplicated_message = data_tools.checked_duplicated(peptides)
+    #print duplicated_message
+
+    for n in [100, 200, 300, 500, 1000, 2000, 3000]:
+        benchmark = ml_tools.rt_benchmark(peptides, 'elude', 'gp', 100, 10)
+        m, s = ml_tools.parallel_cross_validataion(benchmark)
+        print "%d %g %g" % (n, m, s)
+
+
