0185fb26dec552d3b6ca71e336b06794c9957891
Heydar <heydar@gmail.com>
2015-09-01 14:59:50 +0200

    Minor UPdates

--- scr/Package/script.py
+++ scr/Package/script.py
@@ -14,8 +14,8 @@ if __name__ == "__main__":
     # duplicated_message = data_tools.checked_duplicated(peptides)
     # print duplicated_message
 
-    for n in [100, 200, 300, 500, 1000, 1500 ]:
-        benchmark = ml_tools.rt_benchmark(peptides, 'elude', 'gp', n, 10)
+    for n in [100, 200, 300, 500, 1000, 1500, 2000, 2500, 3000 ]:
+        benchmark = ml_tools.rt_benchmark(peptides, 'elude', 'gp', n, 5)
         res = ml_tools.parallel_cross_validataion(benchmark)
 
         means = np.mean( res, axis=0 ).tolist()[0]
