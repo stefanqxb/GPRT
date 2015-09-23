54695e29419bf03caff9650b24edb80961c5f947
Heydar <heydar@gmail.com>
2015-09-23 21:08:22 +0200

    Minor Updates

--- scr/Package/plotting_script.py
+++ scr/Package/plotting_script.py
@@ -1,13 +1,19 @@
 #!/usr/bin/python
 
+import platform
 import numpy as np
 import data_tools
 import pickle as pk
 import ml_tools
 import gp_tools
+from matplotlib import pyplot as pp
+pp.ion()
 
-def load_test_vectors( n = 100 ):
-    data_root = "/Users/heydar/Stuff/tmp/gprt"
+def load_data( n = 100 ):
+    if platform.system() == 'Darwin' :
+        data_root = "/Users/heydar/Stuff/tmp/gprt"
+    else :
+        data_root = "/media/hdd/heydar/data/gprt"
     model_tmp = "%s/models_ntrain_%d.pk"
     path = model_tmp % ( data_root, n )
 
@@ -18,8 +24,7 @@ def load_test_vectors( n = 100 ):
     test_vectors = benchmark.get_test_vectors( 0, models[0] )
     return models[0], kernels[0], test_vectors
 
-def calc_values( kernel, test_vectors ):
-
+def dist_vs_var( kernel, test_vectors ):
     dist = [];
     var = [];
 
@@ -28,4 +33,11 @@ def calc_values( kernel, test_vectors ):
         dist.append( d )
         var.append(t[3])
 
-    return np.array( dist ), np.array( var )
+    dist = np.array( dist )
+    var = np.array( var )
+
+    pp.plot( dist, var, 'r.')
+    pp.grid()
+    pp.xlabel('Distance From Training Data')
+    pp.ylabel('Predicted Variance')
+    pp.title('Distance vs. Variance')
