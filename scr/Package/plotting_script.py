a3d4839bcd38c7e30d34cfc762c6856eafe10021
Heydar <heydar@gmail.com>
2015-09-24 07:16:40 +0200

    Minor Updates

--- scr/Package/plotting_script.py
+++ scr/Package/plotting_script.py
@@ -9,35 +9,40 @@ import gp_tools
 from matplotlib import pyplot as pp
 pp.ion()
 
-def load_data( n = 100 ):
-    if platform.system() == 'Darwin' :
-        data_root = "/Users/heydar/Stuff/tmp/gprt"
-    else :
-        data_root = "/media/hdd/heydar/data/gprt"
-    model_tmp = "%s/models_ntrain_%d.pk"
-    path = model_tmp % ( data_root, n )
+class data_plotter:
+    def __init__( self, n=100 ):
+        self.n = 100
+    def load_data( self ):
+        if platform.system() == 'Darwin' :
+            data_root = "/Users/heydar/Stuff/tmp/gprt"
+        else :
+            data_root = "/media/hdd/heydar/data/gprt"
+        model_tmp = "%s/models_ntrain_%d.pk"
+        path = model_tmp % ( data_root, self.n )
+        self.peptides = data_tools.read_data()
+        self.benchmark = ml_tools.rt_benchmark(peptides, 'elude', 'gp', self.n, 5)
+        self.models, self.kernels = ml_tools.load_rt_models( path )
 
-    peptides = data_tools.read_data()
-    benchmark = ml_tools.rt_benchmark(peptides, 'elude', 'gp', n, 5)
-    models, kernels = ml_tools.load_rt_models( path )
+        test_vectors = benchmark.get_test_vectors( 0, models[0] )
 
-    test_vectors = benchmark.get_test_vectors( 0, models[0] )
-    return models[0], kernels[0], test_vectors
+        return models[0], kernels[0]
 
-def dist_vs_var( kernel, test_vectors ):
-    dist = [];
-    var = [];
+    def dist_vs_var( pind=0 ):
+        test_vectors = self.benchmark.get_test_vectors(pind,self.models[pind])
+        dist = [];
+        var = [];
 
-    for t in test_vectors : 
-        d = kernel.mean_dist( t[0] )
-        dist.append( d )
-        var.append(t[3])
+        for t in test_vectors : 
+            d = self.kernels[pind].mean_dist( t[0] )
+            dist.append( d )
+            var.append(t[3])
 
-    dist = np.array( dist )
-    var = np.array( var )
+        dist = np.array( dist )
+        var = np.array( var )
 
-    pp.plot( dist, var, 'r.')
-    pp.grid()
-    pp.xlabel('Distance From Training Data')
-    pp.ylabel('Predicted Variance')
-    pp.title('Distance vs. Variance')
+        pp.plot( dist, var, 'r.')
+        pp.grid()
+        pp.xlabel('Distance From Training Data')
+        pp.ylabel('Predicted Variance')
+        pp.title('Distance vs. Variance')
+        pp.savefig('dist_vs_var.pdf')
