1fbe13a2ab2bd8bf43a3418e228bf9cc4a064c1a
Heydar <heydar@gmail.com>
2015-10-15 19:16:44 +0200

    Minor Updates

--- scr/Package/common.py
+++ scr/Package/common.py
@@ -10,11 +10,11 @@ class parameters:
         else :
             self.data_root = "/media/hdd/heydar/data/gprt"
 
-        #self.data_path = './data/20121212_S25_3ug_300min_LCMS_PM3_Tryp_GRADIENT_15sec_MZ.rtimes.tsv'
-        #self.data_name = 'no_modif'
+        self.data_path = './data/20121212_S25_3ug_300min_LCMS_PM3_Tryp_GRADIENT_15sec_MZ.rtimes.tsv'
+        self.data_name = 'no_modif'
 
-        self.data_path = './data/20120126_EXQ5_KiSh_SA_LabelFree_HeLa_Phospho_Control_rep1_Fr3.rtimes.tsv'
-        self.data_name = 'ptm_modif'
+        #self.data_path = './data/20120126_EXQ5_KiSh_SA_LabelFree_HeLa_Phospho_Control_rep1_Fr3.rtimes.tsv'
+        #self.data_name = 'ptm_modif'
 
         self.models_name = 'original'
         self.models_tag = '%s_%s' % ( self.models_name, self.data_name )
@@ -24,6 +24,6 @@ class parameters:
             os.mkdir( self.models_path )
 
         self.save_tmp = '%s/%s/models_ntrain_%d.pk'
-        self.ntrain = [ 100, 200, 300, 400, 500, 1000 ];
+        self.ntrain = [ 100, 200, 300, 400, 500, 600, 700, 800, 900, 1000 ];
         self.nparts = 10
         self.train_ratio = 0.5
